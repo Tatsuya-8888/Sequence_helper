aa51acc81a0d5303445a85c48d87b22a5b5495a7
unknown <t-ishii@2w850h3.watty.local>
2019-07-01 13:01:16 +0900

    first commit

---
+++ sqh.py
@@ -0,0 +1,29 @@
+import socket
+
+class Seq_help:
+    IP      =   "192.168.1.1"
+    PORT    =   5015
+
+    def __init__(self,IP,PORT):
+        self.IP = IP
+        self.PORT = PORT
+        # 接続処理
+
+
+    def reconnect():
+        # 再接続処理
+        return 0
+
+    def Word_read():
+        return 0
+        #D B などの ワード単位リード
+
+    def Bit_read():
+        # M Y X などの ビット単位リード
+
+    
+    def Word_write():
+
+    
+    def Bit_write():
+
