5858553553fc51394d7f2fc47b44e9cf69a8a68a
TATSUYA ISHII <tatsuya@TATSUYA-MacBookPro.local>
2019-07-01 14:18:20 +0900

    write open command

--- sqh.py
+++ sqh.py
@@ -1,5 +1,7 @@
 import socket
 
+
+
 class Seq_help:
     IP      =   "192.168.1.1"
     PORT    =   5015
@@ -8,22 +10,30 @@ class Seq_help:
         self.IP = IP
         self.PORT = PORT
         # 接続処理
+        client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        client.connect((ip,port))
 
 
-    def reconnect():
+
+    def reconnect(self):
         # 再接続処理
         return 0
 
-    def Word_read():
+    def Word_read(self):
         return 0
         #D B などの ワード単位リード
 
-    def Bit_read():
+    def Bit_read(self):
         # M Y X などの ビット単位リード
+        return 0
 
-    
-    def Word_write():
+    def Word_write(self):
+        # ワード書き込み
+        return 0
 
-    
-    def Bit_write():
+
+    def Bit_write(self):
+        # ビット書き込み
+        return 0
+
 
